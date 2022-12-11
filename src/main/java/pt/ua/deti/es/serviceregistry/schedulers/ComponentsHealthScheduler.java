5cd7fb1ff36bd10f5ac6a468e67d5e2e8fa351d4
Hugo1307 <hugomiguel_2001_@sapo.pt>
2022-12-11 15:39:53 +0000

    Fix: Fixed Health check for UIs.

--- src/main/java/pt/ua/deti/es/serviceregistry/schedulers/ComponentsHealthScheduler.java
+++ src/main/java/pt/ua/deti/es/serviceregistry/schedulers/ComponentsHealthScheduler.java
@@ -4,16 +4,21 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
+import pt.ua.deti.es.serviceregistry.data.dto.RegisteredComponentDto;
 import pt.ua.deti.es.serviceregistry.entities.ComponentAvailability;
-import pt.ua.deti.es.serviceregistry.utils.ComponentUtils;
+import pt.ua.deti.es.serviceregistry.entities.ComponentType;
 import pt.ua.deti.es.serviceregistry.entities.HealthReport;
+import pt.ua.deti.es.serviceregistry.utils.ComponentUtils;
 import pt.ua.deti.es.serviceregistry.web.services.RegistryWebService;
 
 import java.time.Duration;
@@ -49,36 +54,49 @@ public class ComponentsHealthScheduler {
 
             String componentHealthEndpoint = componentUtils.buildHealthEndpoint(registeredComponent, false);
 
-            HealthReport componentHealthReport;
+            ResponseEntity<HealthReport> componentHealthReport;
 
             try {
-                componentHealthReport = restTemplate.getForObject(componentHealthEndpoint, HealthReport.class);
-            } catch (RestClientException e) {
-                // Ignore Exception. Already treated in the next if statement.
+                componentHealthReport = restTemplate.exchange(componentHealthEndpoint, HttpMethod.GET, null, HealthReport.class);
+            } catch (ResourceAccessException e) {
+                // Ignore Timeout Exception. Already treated in the next if statement.
                 componentHealthReport = null;
             }
 
-            if (componentHealthReport != null && componentHealthReport.isHealthy()) {
-                registryWebService.updateAvailabilityStatus(registeredComponent.getId(), ComponentAvailability.ONLINE);
+            boolean isComponentHealthy;
+
+            if (registeredComponent.getComponentType() == ComponentType.UI) {
+                isComponentHealthy = componentHealthReport != null && componentHealthReport.getStatusCode() == HttpStatus.OK;
             } else {
-
-                long lastTimeHealthyTimestamp = registeredComponent.getComponentAvailability().getLastTimeOnline();
-
-                if (registeredComponent.getComponentAvailability().getAvailability() == ComponentAvailability.OFFLINE) {
-                    return;
-                }
-
-                if (System.currentTimeMillis() - lastTimeHealthyTimestamp > availabilityOfflineTimeout) {
-                    registryWebService.updateAvailabilityStatus(registeredComponent.getId(), ComponentAvailability.OFFLINE);
-                    log.warn(String.format("Component %s (%s) did not respond for 1 minute - Marked as Offline.", registeredComponent.getComponentName(), registeredComponent.getId()));
-                } else {
-                    registryWebService.updateAvailabilityStatus(registeredComponent.getId(), ComponentAvailability.NOT_RESPONDING);
-                    log.warn(String.format("Component %s (%s) is not responding.", registeredComponent.getComponentName(), registeredComponent.getId()));
-                }
-
+                isComponentHealthy = componentHealthReport != null && componentHealthReport.getStatusCode() == HttpStatus.OK && componentHealthReport.getBody() != null && componentHealthReport.getBody().isHealthy();
             }
 
+            updateComponentStatus(registeredComponent, isComponentHealthy);
+
         });
+
+    }
+
+    private void updateComponentStatus(RegisteredComponentDto registeredComponent, boolean isHealthy) {
+
+        if (isHealthy) {
+            registryWebService.updateAvailabilityStatus(registeredComponent.getId(), ComponentAvailability.ONLINE);
+        } else {
+            long lastTimeHealthyTimestamp = registeredComponent.getComponentAvailability().getLastTimeOnline();
+
+            if (registeredComponent.getComponentAvailability().getAvailability() == ComponentAvailability.OFFLINE) {
+                return;
+            }
+
+            if (System.currentTimeMillis() - lastTimeHealthyTimestamp > availabilityOfflineTimeout) {
+                registryWebService.updateAvailabilityStatus(registeredComponent.getId(), ComponentAvailability.OFFLINE);
+                log.warn(String.format("Component %s (%s) did not respond for 1 minute - Marked as Offline.", registeredComponent.getComponentName(), registeredComponent.getId()));
+            } else {
+                registryWebService.updateAvailabilityStatus(registeredComponent.getId(), ComponentAvailability.NOT_RESPONDING);
+                log.warn(String.format("Component %s (%s) is not responding.", registeredComponent.getComponentName(), registeredComponent.getId()));
+            }
+
+        }
 
     }
 
